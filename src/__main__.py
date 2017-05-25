ee822b8524cda41cab3c7174a43bf7e061482e3a
Nicholas Carr <nicholas.carr@rogers.com>
2017-05-25 16:50:48 -0400

    The code works now

--- src/__main__.py
+++ src/__main__.py
@@ -4,13 +4,18 @@ Nicholas Carr and Victor Lin
 2017-05-25
 """
 
-from pygame import *
+import pygame
+import math
+from pygame.locals import *
+SCREEN = pygame.display.set_mode((1010, 695))
+CLOCK = pygame.time.Clock()
 
-class Car(sprite.Sprite):
+class Car(pygame.sprite.Sprite):
     """All cars inherit this class"""
     MAX_SPEED = 20
-    def __init__(self, coordinates, speed, direction):
-        self.coordinates = coordinates
+    def __init__(self, position, speed, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.position = position
         self.speed = speed
         self.direction = direction
 
@@ -22,14 +27,42 @@ class UserCar(Car):
 
 class DummyCar(Car):
     """The cars that drive independently of the user"""
+    def __init__(self, scroller, position, speed, direction):
+        Car.__init__(self, position, speed, direction)
+        self.scroller = scroller
+
+    def update(self, deltat):
+        x, y = self.position
+        rad = self.direction * math.pi / 180
+        x += -self.speed * math.sin(rad)
+        y += -self.speed * math.cos(rad)
+        self.position = (x, y)
+        self.image = pygame.transform.rotate(pygame.image.load("../assets/png/playerCar.png"), self.direction)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.position
 
 class BackgroundScroller(object):
     """Scrolls the background at the speed the car is going"""
     def __init__(self):
-        self.cars = sprite.Group()
+        self.cars = pygame.sprite.Group()
 
     def addDummyCar(self, coordinates, speed, direction):
         """Create a DummyCar and add it to the cars group"""
         dummyCar = DummyCar(self, coordinates, speed, direction)
-        cars.add(dummyCar)
+        self.cars.add(dummyCar)
         return dummyCar
+
+scroller = BackgroundScroller()
+car = scroller.addDummyCar((512, 300), 1, 0)
+while True:
+    deltat = CLOCK.tick(30)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    # RENDERING
+    SCREEN.fill((0,0,0))
+    scroller.cars.update(deltat)
+    scroller.cars.draw(SCREEN)
+    pygame.display.flip()
