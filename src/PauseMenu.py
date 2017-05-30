4eea4db697cd9b3d7786b80f82df8d9e65f1ccd8
celery <vlvictorlin@gmail.com>
2017-05-30 10:34:11 -0400

    Added comments to PauseMenu

--- src/PauseMenu.py
+++ src/PauseMenu.py
@@ -1,11 +1,13 @@
 import pygame
 
-#class Pause(object):
 def Pause(CLOCK, SCREEN):
+    # Load the image and set the size and position to overlay the screen
     image = pygame.image.load("../assets/png/pause.png")
     rect = image.get_rect()
     rect.topleft = (0, 0)
+    # Draw the image on top of the screen
     SCREEN.blit(image, rect)
+    # Update the screen
     pygame.display.flip()
     while True:
         CLOCK.tick(60)
@@ -13,5 +15,6 @@ def Pause(CLOCK, SCREEN):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            # Check to see if the player has unpaused the game
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 return True
