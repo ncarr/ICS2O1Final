a18e9c60aa0766fb8d7f6574d3d67316ab3a11c2
celery <vlvictorlin@gmail.com>
2017-06-02 15:00:55 -0400

    i need help

---
+++ src/Help.py
@@ -0,0 +1,27 @@
+import pygame
+from Button import Button
+from EventLoop import EventLoop
+
+class Help(object):
+    def __init__(self, SCREEN):
+        # Load the image and set the size and position
+        image = pygame.image.load("../assets/png/help.png")
+        rect = image.get_rect()
+        rect.topleft = (0, 0)
+        # Create the back button and draw it onto the image
+        backButton = Button((400, 100), (505, 590), "Back")
+        # Draw the image on top of the screen
+        SCREEN.blit(image, rect)
+        # Output the first frame
+        pygame.display.flip()
+        # Create a new event loop
+        loop = EventLoop()
+        # Go back
+        backButton.onClick(loop.stop)
+        # When a new frame needs to be rendered, update the button
+        @loop.onUpdate
+        def update():
+            backButton.update()
+            SCREEN.blit(backButton.image, backButton.rect)
+        # Start animating
+        loop.start()
--- src/MainMenu.py
+++ src/MainMenu.py
@@ -1,17 +1,21 @@
 import pygame
 from Button import Button
 from EventLoop import EventLoop
+from Help import Help
 
 class MainMenu(object):
     def __init__(self, SCREEN):
         # Load the image and set the size and position to overlay the screen
         image = pygame.image.load("../assets/png/mainMenu.png")
-        # Create the play button and draw it onto the image
-        playButton = Button((400, 100), (505, 400), "Play")
-        image.blit(playButton.image, playButton.rect)
-        # Find the image's position
         rect = image.get_rect()
         rect.topleft = (0, 0)
+        # Create the play button and draw it onto the image
+        playButton = Button((400, 100), (505, 355), "Play")
+        helpButton = Button((400, 100), (505, 475), "Help")
+        quitButton = Button((400, 100), (505, 590), "Quit")
+        image.blit(playButton.image, playButton.rect)
+        image.blit(helpButton.image, helpButton.rect)
+        image.blit(quitButton.image, quitButton.rect)
         # Draw the image on top of the screen
         SCREEN.blit(image, rect)
         # Output the first frame
@@ -20,10 +24,24 @@ class MainMenu(object):
         loop = EventLoop()
         # Continue to the game if they click play
         playButton.onClick(loop.stop)
+        # Exit the game
+        @quitButton.onClick
+        def quit():
+            pygame.quit()
+            exit()
+        # Help screen
+        @helpButton.onClick
+        def help():
+            Help(SCREEN)
+            print ('literally anything')
         # When a new frame needs to be rendered, update the button
         @loop.onUpdate
         def update():
             playButton.update()
             SCREEN.blit(playButton.image, playButton.rect)
+            helpButton.update()
+            SCREEN.blit(helpButton.image, helpButton.rect)
+            quitButton.update()
+            SCREEN.blit(quitButton.image, quitButton.rect)
         # Start animating
         loop.start()
