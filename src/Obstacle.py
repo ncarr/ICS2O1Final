0e86860dbed47e6053762d80113cd71d623d0ddf
celery <vlvictorlin@gmail.com>
2017-06-05 15:28:54 -0400

    Added comments

--- src/Obstacle.py
+++ src/Obstacle.py
@@ -1,5 +1,6 @@
 from random import choice, randint
 class Obstacle(object):
+    # Positions of centers of lanes
     lanes = [298, 436, 574, 712]
     def __init__(self, game):
         self.game = game
@@ -7,13 +8,17 @@ class Obstacle(object):
     def drawObject(self):
         x = choice(self.__class__.lanes)
         speed = 10
+        # Determine which lanes the cars are in
         if x > 505:
             direction = 0
         else:
             direction = 180
+        # Draw the car
         self.game.scroller.addDummyCar((x,-150), speed, direction)
+    # Determine when the object should appear
     def createObject(self):
         self.objectDistance = self.game.maxDistance + randint(2000,4000)
+    # Draw the object
     def update(self):
         if self.game.maxDistance >= self.objectDistance:
             self.drawObject()
