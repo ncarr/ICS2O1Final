a605bc088ba043384b17a6d649557770a92d1b5b
celery <vlvictorlin@gmail.com>
2017-06-08 15:07:03 -0400

    aded suport for QUTOES

--- src/Sound.py
+++ src/Sound.py
@@ -1,9 +1,9 @@
 import pygame
 class Sound(object):
     def __init__(self):
-        self.backgroundMusic = pygame.mixer.Sound(music.mp3)
-        self.hornSound = pygame.mixer.Sound(horn.mp3)
-        self.obstacleSound = pygame.mixer.Sound(collide.mp3)
+        self.backgroundMusic = pygame.mixer.Sound('music.mp3')
+        self.hornSound = pygame.mixer.Sound('horn.mp3')
+        self.obstacleSound = pygame.mixer.Sound('collide.mp3')
     def Music(self):
         self.backgroundMusic.play(loops=-1)
     def Collide(self, type):
