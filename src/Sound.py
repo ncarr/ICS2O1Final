e3680b67b363221055f6630e7c2b19ac3d37e5c6
celery <vlvictorlin@gmail.com>
2017-06-08 15:05:45 -0400

    aded suport for SOUDN

---
+++ src/Sound.py
@@ -0,0 +1,13 @@
+import pygame
+class Sound(object):
+    def __init__(self):
+        self.backgroundMusic = pygame.mixer.Sound(music.mp3)
+        self.hornSound = pygame.mixer.Sound(horn.mp3)
+        self.obstacleSound = pygame.mixer.Sound(collide.mp3)
+    def Music(self):
+        self.backgroundMusic.play(loops=-1)
+    def Collide(self, type):
+        if collideType == 'car':
+            self.hornSound.play(loops=0)
+        elif collideType == 'object':
+            self.obstacleSound.play(loops=0)
