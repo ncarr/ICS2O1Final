07bf13c135e8648daf9dc852f6f88c5efdc7e59c
Nicholas Carr <nicholas.carr@rogers.com>
2017-06-12 21:39:24 -0400

    Fixed build tools

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"build_exe":"../dist", "include_files": ["help.png", "icon.png", "mainMenu.png", "pause.png", "playerCar.png", "policeCar.png", "road.png", "trafficCone.png"]}
+build_exe_options = {"build_exe":"../dist", "include_files": ["help.png", "icon.png", "mainMenu.png", "pause.png", "playerCar.png", "policeCar.png", "road.png", "trafficCone.png", "blueCar.png", "collide.ogg", "countdownbg.png", "endScreen.png", "greenCar.png", "highScore.txt", "horn.ogg", "orangeCar.png", "purpleCar.png", "siren.ogg", "yellowCar.png"]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
